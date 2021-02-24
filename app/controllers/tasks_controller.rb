4b7250a08e8d2c8cfae7134f2387dfbb404f5acf
Phil Lee <prodigy7748g@gmail.com>
2021-02-24 13:35:22 +0800

    done task create action

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,4 +6,19 @@ class TasksController < ApplicationController
   def new
     @task = Task.new
   end
+
+  def create
+    @task = Task.new(task_params)
+
+    if @task.save
+      redirect_to tasks_path, notice: '新增任務成功！'
+    else
+      render :new
+    end
+  end
+
+  private
+  def task_params
+    params.require(:task).permit(:title, :content)
+  end
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,2 @@
 class Task < ApplicationRecord
-  belongs_to :User
 end
