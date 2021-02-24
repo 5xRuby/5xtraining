077545d74b5b0578b2b1d98feed7b44dbc250ecd
Phil Lee <prodigy7748g@gmail.com>
2021-02-24 13:57:25 +0800

    done task edit & update action

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,6 @@
 class TasksController < ApplicationController
+  before_action :find_task, only:[:edit, :update, :show]
+
   def index
     @tasks = Task.all
   end
@@ -17,8 +19,23 @@ class TasksController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @task.update(task_params)
+      redirect_to tasks_path, notice: '修改任務成功！'
+    else
+      render :edit
+    end
+  end
+
   private
   def task_params
     params.require(:task).permit(:title, :content)
+  end
+
+  def find_task
+    @task = Task.find(params[:id])
   end
 end
